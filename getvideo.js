09985a77fcfeee7ddc05a1fca9f2f756c6c23543
Jianxin Zhong (azuwis) <azuwis@gmail.com>
2008-12-30 01:44:59 +0800

    tudou: rename tudou to getvideo, prepare for generic getvideo usage

--- getvideo.js
+++ getvideo.js
@@ -1,5 +1,5 @@
 CmdUtils.CreateCommand({
-    name: "tudou",
+    name: "getvideo",
     icon: "http://www.tudou.com/favicon.ico",
     homepage: "http://vi.appspot.com/",
     author: {
@@ -10,17 +10,12 @@ CmdUtils.CreateCommand({
     description: "Tudou download list",
     help: "Just preview the command, download list will be copied to clipboard when success.",
     //takes: {"input": noun_arb_text},
-    preview: function(pblock, input) {
-        var doc = CmdUtils.getDocument();
-        var uri = Utils.url(doc.documentURI);
-        if (uri.spec.match("http://www.tudou.com/playlist/playindex.do")) {
-            pblock.innerHTML = "Working...";
-        } else {
-            pblock.innerHTML = "Preview this command in tudou playlist page.";
-            return;
-        }
+    _handleTudouPlaylist: function(pblock, doc) {
+        pblock.innerHTML = "Working...";
+        var iidDoc = jQuery(doc).find("div#slidePlaylist li");
+        var iidMax = iidDoc.length;
         var list = [];
-        jQuery(doc).find("div#slidePlaylist li").each(function(i) {
+        iidDoc.each(function(i) {
             var iid = this.id.substring(8);
             jQuery.ajax({
                 url: 'http://v2.tudou.com/v2/cdn?id=' + iid,
@@ -33,14 +28,27 @@ CmdUtils.CreateCommand({
                     };
                 }
             });
+            pblock.innerHTML = "Working..." + i + "/" + iidMax;
         });
+        return list;
+    },
+    preview: function(pblock, input) {
+        var doc = CmdUtils.getDocument();
+        var uri = Utils.url(doc.documentURI);
+        var list = [];
+        if (uri.spec.match("http://www.tudou.com/playlist/playindex.do")) {
+            list = this._handleTudouPlaylist(pblock, doc);
+        } else {
+            pblock.innerHTML = "Preview this command in tudou playlist page.";
+            return;
+        }
         var ariaList = "";
         jQuery.each(list,
         function() {
             ariaList += this.url + "\n" + "  out=" + this.title + ".flv\n";
         });
         CmdUtils.copyToClipboard(ariaList);
-        pblock.innerHTML = "Working...Success! " + list.length + " items copied to clipboard.";
+        pblock.innerHTML = "Working...Done! " + list.length + " items copied to clipboard.";
     },
     execute: function(input) {
         CmdUtils.setSelection("You selected: " + input.html);
